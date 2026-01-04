d1c60cd6e109dc141d9d7dd22e99c3c413288c28
Aiswarya0302 <ishuu0317@gmail.com>
2026-01-04 22:34:34 +0530

    Add files via upload

---
+++ Nodefaul.java
@@ -0,0 +1,30 @@
+package assingment03;
+
+public class Nodefaul {
+
+	
+		
+		
+			/*
+			void show() {
+			//Without Initialize a value
+			int l;
+			System.out.println("Without Initializing a value: "+l);
+			}
+			*/
+	
+			//With initialize a value
+			String local="Local variable with initialzing a value";
+			
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				Nodefaul lvd=new Nodefaul();
+				//lvd.show();
+				System.out.println("With Initializing a  value:   "+lvd.local);
+
+			}
+
+		}
+	
+
+
