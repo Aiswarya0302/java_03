4936d97dd6a61fca468e53f1f094417234ddda80
Aiswarya0302 <ishuu0317@gmail.com>
2026-01-04 22:25:25 +0530

    Add files via upload

---
+++ StaticVar.java
@@ -0,0 +1,23 @@
+package assingment03;
+
+public class StaticVar {
+
+		
+			static String a="JK";
+			
+
+			public static void main(String[] args) {
+				
+				
+				//Accessing Static Varibale using classname.fieldname=value;
+				StaticVar.a="Bunny";
+				
+				System.out.println("Access Static Variable using classname:  "+StaticVar. a);
+
+			}
+
+		
+
+	}
+
+
