889052f4cf3692c832637ced59bcc5b12c62ddb0
Aiswarya0302 <ishuu0317@gmail.com>
2026-01-04 22:22:37 +0530

    Add files via upload

---
+++ LocalVar.java
@@ -0,0 +1,24 @@
+package assingment03;
+
+public class LocalVar{
+	void sum()
+	{
+		int a=20,b=40;
+		
+		int c=a+b;
+		
+		System.out.println(c);
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		LocalVar lv=new LocalVar();
+		
+		lv.sum();
+		
+		
+	}
+
+}
